f382e56214fe0d7f98ea54b23e75de80727f3ab0
Tisca Catalin <admin@tiscacatalin.com>
2025-11-01 17:54:20 +0200

    design adjusmnets

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,65 +1,38 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <main className="flex flex-col gap-16 justify-center items-center w-screen min-h-screen">
+      <div className="space-y-4 text-center">
+        <h1 className="text-8xl font-bold text-zinc-600 font-medium">fund loop</h1>
+        <p>decentralized funding through deposits</p>
+      </div>
+
+      <div className="flex gap-4">
+        <Button variant="default">connect</Button>
+        <Button variant="outline">about</Button>
+      </div>
+
+      <div className="grid max-w-3xl w-full w-full grid-cols-1 sm:grid-cols-[1fr_1px_1fr] gap-12">
+        <div className="w-full h-96 bg-zinc-900 rounded-lg shadow p-12 flex flex-col justify-end gap-4 hover:scale-105 transition-all">
+          <h2 className="text-purple-400 font-bold text-2xl">deposit & fund</h2>
+          <p className="text-white">
+            deposit your crypto to fund others in need & benefit from receiving
+            cred through badges & rewards
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="w-[1px] h-96 py-12">
+          <div className="w-full h-full bg-zinc-300 rounded-full"></div>
         </div>
-      </main>
-    </div>
+        <div className="w-full h-96 bg-zinc-900 rounded-lg shadow p-12 flex flex-col justify-end gap-4 hover:scale-105 transition-all">
+          <h2 className="text-purple-400 font-bold text-2xl">find support</h2>
+          <p className="text-white">
+            post about what you're building & find the funding you need
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
